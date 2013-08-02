42a93230635b96c7a60d21de25d696decc391f15
Michal Horcic <michalhorcic@gmail.com>
2013-08-02 23:03:47 +0200

    small edit2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Mblog::Application.routes.draw do
   resources :users
 
-  match '/users',  to: 'users'
+  match '/users',  to: 'users#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
