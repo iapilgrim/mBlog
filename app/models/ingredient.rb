d79860f44a203a627ac82ca25ec83a3ba8acce5d
Michal Horcic <michalhorcic@gmail.com>
2013-08-16 00:14:39 +0200

    Editing mistake

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,6 @@ class Ingredient < ActiveRecord::Base
   attr_accessible :description, :name
   has_many :ingredient_in_recipes, :dependent => :destroy
   has_many :ingredient_in_steps, :dependent => :destroy
-  has_many :recipes, through => :ingredient_in_recipes
-  has_many :steps, through => :ingredient_in_steps
+  has_many :recipes, :through => :ingredient_in_recipes
+  has_many :steps, :through => :ingredient_in_steps
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ActiveRecord::Base
   attr_accessible :description, :name
   has_many :ingredient_in_recipes, :dependent => :destroy
-  has_many :ingredients, through => :ingredient_in_recipes
+  has_many :ingredients, :through => :ingredient_in_recipes
   has_many :steps, :dependent => :destroy
 end
