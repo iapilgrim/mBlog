86ff4156c769b8cd870a39eb2ef4ca92fc9bd6f4
Michal Horčic <michalhorcic@gmail.com>
2014-02-27 16:17:32 +0100

    first feature test

--- spec/features/user_authetication_spec.rb
+++ spec/features/user_authetication_spec.rb
@@ -5,8 +5,20 @@ feature 'user authentication' do
     # add setup details
   end
 
-  scenario 'user signing up' do 
-    pending "waiting for implementation"
+  scenario 'user signing up' do
+    visit root_path
+
+    expect{
+    click_link 'Sign up'
+    fill_in 'Email', with: 'newuser@example.com'
+    fill_in 'Password', with: 'Secret123'
+    fill_in 'Password confirmation', with: 'Secret123'
+    click_button 'Sign up'}.to change(User, :count).by(1)
+
+    # rewrite exectation to expect current_user path - user is redirected to his/her profile after signing up
+    expect(current_path).to eq users_path
+    expect(page).to have_content 'Successfully signed up'
+    # pending "waiting for implementation"
     # write the example!
   end
 end
